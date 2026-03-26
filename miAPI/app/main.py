5317f4ffe1c888d1258e5aa959b8ec520cd365ab
Arviziuu <162623489+Arviziuu@users.noreply.github.com>
2026-03-26 16:54:27 -0600

    Implementacion de SQLAlchemy
    Se reorganizo la estructura del proyecto para un mejor manejo y se impllemento SQLAlchey

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,18 +1,18 @@
 #1.importaciones
 from fastapi import FastAPI
-from app.routers import varios, usuarios
+from app.routers import usuarios, varios
+from app.data.db import engine
+from app.data import usuario
 
+usuario.Base.metadata.create_all(bind=engine)
 
-#2. Inicialización APP
-app = FastAPI()
+app=FastAPI()
 
-#3. Endpoints
 app=FastAPI(
     title='Mi primer API', 
-    description="Ivan Moreno",
+    description="Diego Arvizu",
     version='1.0.0'
     )
 
 app.include_router(usuarios.routerU)
 app.include_router(varios.routerV)
-
